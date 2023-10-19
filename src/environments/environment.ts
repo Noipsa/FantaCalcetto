f6edd84771404c61a7db2d7c4499a02b70fa21bb
Noipsa <117486477+Noipsa@users.noreply.github.com>
2023-10-19 19:25:56 +0200

    FC-3 - Firebase Deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  apiKey: "AIzaSyCJdvK2z5E0IKdfQxIC_EgXrf-FJ9_qzvA",
+  authDomain: "fantacalcetto-7f4c0.firebaseapp.com",
+  projectId: "fantacalcetto-7f4c0",
+  storageBucket: "fantacalcetto-7f4c0.appspot.com",
+  messagingSenderId: "505592692499",
+  appId: "1:505592692499:web:d4f138902319aa164764e6",
+  measurementId: "G-PKDR94NC9B"
 };
