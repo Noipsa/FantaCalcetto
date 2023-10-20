8dc42a989f7ea8ccf8cff8a114c303c5da9b9450
Noipsa <117486477+Noipsa@users.noreply.github.com>
2023-10-20 22:29:44 +0200

    FC - 9 - Funzioni Amministratore

--- src/app/amministrazione/amministrazione.component.ts
+++ src/app/amministrazione/amministrazione.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./amministrazione.component.scss']
 })
 export class AmministrazioneComponent {
+  utenti: any = [];
+  giocatori: any = [];
 
+  options: any = [];
+  searchedOptions: any;
+  giocatoreSelected: any = 'scegli giocatore';
+
+  onSeachDropdownValue(event: any) {
+    const value = event.target.value;
+    this.searchedOptions = this.options.filter((option: string | any[]) => option.includes(value));
+  }
+
+  onSelectDropdownValue(option: any) {
+    this.giocatoreSelected = option;
+  }
 }
